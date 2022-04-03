e581837469a9de4e31c5e8a779298c2f0cf87735
matemaptichka <nasty.internal@gmail.com>
2022-04-03 23:48:31 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String [] args) {
+        int price = 1500;
+        int discount = 20;
+
+        System.out.println("Количество начисленных бонусных миль " + (price/discount));
+    }
 }
